31cc4efa6a569c4191ceb59788893a1b157954de
sabujghorai <ghoraisabuj4@gmail.com>
2026-07-27 22:45:32 +0530

    new change

--- Career_Mentor/Admin/admin_dashboard.php
+++ Career_Mentor/Admin/admin_dashboard.php
@@ -43,8 +43,5 @@ if (!isset($_SESSION['id']) || $_SESSION['role'] !== 'admin') {
     </div>
 
 </body>
-<<<<<<< HEAD
+
 </html>
-=======
-</html>
->>>>>>> 7c8bb21 (updated the admin_dashboard file)
